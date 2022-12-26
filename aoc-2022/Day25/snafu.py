21b9657142eee7e8e478cb24358c37f053c5f02a
Sam DeCook <45473144+sam-decook@users.noreply.github.com>
2022-12-25 23:59:30 -0500

    Day25 pt1

--- aoc-2022/Day25/snafu.py
+++ aoc-2022/Day25/snafu.py
@@ -1,8 +1,10 @@
+"""------------------------------- Functions -------------------------------"""
+# Converts a SNAFU number (modified base-5) to base-10 (decimal)
 def convert(line):
     base10 = 0
     digits = len(line)
 
-    # Reverse string to make parsing easier
+    # Reverse string to make parsing easier - index is power (5 ^ i)
     line = line[::-1]
     for i in range(digits):
         num = 0
@@ -20,10 +22,33 @@ def convert(line):
 #   (if there is one)
 # BUT, that might make the previous number greater than 2, so
 #   this is called recursively until the biggest number
-def updatePrev(i, base5):
-    pass
+# If the first (biggest) number is greater than 2, a new place is added
+#   Then, this will return 1, otherwise, it returns 0
+def update(i):
+    # The only clean way to modify the array in recursion
+    global base5    
+
+    # If 0,1,2, no changes need to be made
+    if (base5[i]) < 3: return 0
+
+    # Keep track of this for the original loop, otherwise the index
+    #   will become out-of-date and point to the wrong number
+    added = 0
+
+    # If there is no previous number, we need to add one
+    if i == 0: 
+        base5.insert(i, 0)
+        i += 1
+        added = 1
+    
+    base5[i] = base5[i] - 5 # Turn 3 to -2, 4 to -1
+    base5[i-1] += 1         # Increment previous
+
+    if base5[i-1] < 3: return added         # Done with recursion
+    else:              return update(i-1)   # Need to continue fixing
 
 
+"""---------------------------------- Code ----------------------------------"""
 # Get input
 with open("Day25\input.txt", "r") as input:
     lines = input.readlines()
@@ -37,10 +62,9 @@ for line in lines:
 # First, figure out biggest place that fits (34 fits into 25's place)
 # -> 34 / 5^0 > 1    34 / 5^1 > 5    34 / 5^2 > 1    34 / 5^3 < 1
 #                                           ^
-#num = sum
-num = 198
+num = sum
 places = 0 
-while num / (5 ** places) > 1: places += 1
+while num / (5 ** places) >= 1: places += 1
 places -= 1 #overshot, move back one
 
 # Convert to a base-5 intermediate
@@ -51,12 +75,17 @@ for place in range(places, -1, -1): #count backwards to 0
     base5.append(amt)
     num -= amt * place_amount
 
-# 198 -> 1243
 # Convert base-5 to a SNAFU number
 i = 0
 while i < len(base5):
-    if base5[i] > 2:
-        updatePrev(i, base5)
-        base5[i] = base5[i] - 5 #turns 3 to -2, 4 to -1
+    placeAdded = update(i)
+    i += 1 + placeAdded
 
-    pass
+# Convert to string and change -1 and -2 to '-' and '='
+snafu = ""
+for num in base5:
+    if num == -1:   snafu += '-'
+    elif num == -2: snafu += '='
+    else:           snafu += str(num)
+
+print(snafu)
